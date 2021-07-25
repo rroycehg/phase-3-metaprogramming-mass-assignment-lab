abaa473f618e407417b3b2e5a2193260f7ecadcf
Roli <rroycehg@gmail.com>
2021-07-25 15:52:29 -0400

    Completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  # your code here
+  
+  def initialize(avi_attributes)
+    avi_attributes.each do | key, value |
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end 
+
+  end  
+
 end
